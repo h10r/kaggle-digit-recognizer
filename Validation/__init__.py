9e01ec5fcbb5a6723e812acad22c61c0303b422f
hendrik <hendrikheuer@gmail.com>
2014-01-28 23:27:57 +0100

    added validation code for sample sets

--- Validation/__init__.py
+++ Validation/__init__.py
@@ -1,4 +1,5 @@
 import csv
+from random import randint
 
 class Validation():
 
@@ -6,26 +7,34 @@ class Validation():
 	RF_BENCHMARK = "data/rf_benchmark.csv"
 
 	def for_testing():
-		caption,dataset = Validation.load_csv( Validation.KNN_BENCHMARK )
+		caption,dataset = Validation.load_csv( Validation.RF_BENCHMARK )
 		return dataset
 
-	def validate_with_knn(results):
-		caption,dataset = Validation.load_csv( Validation.KNN_BENCHMARK )
+	def validate_with_knn(train_set):
+		caption,validation_set = Validation.load_csv( Validation.KNN_BENCHMARK )
 
-		if ( len(dataset) == len(results) ):
-			print( len(dataset) )
+		if ( len(validation_set) == len(train_set) ):
+			print( Validation.compare_two_lists( validation_set, train_set, len( validation_set ) ) )
 		else:
-			print( "Dataset and results don't match" )
+			print( "validation_set and train_set don't match" )
 			return
 
-	def validate_with_rf(results):
-		caption,dataset = Validation.load_csv( Validation.RF_BENCHMARK )
+	def validate_with_rf(train_set):
+		caption,validation_set = Validation.load_csv( Validation.RF_BENCHMARK )
 
-		if ( len(dataset) == len(results) ):
-			print( len(dataset) )
+		if ( len(validation_set) == len(train_set) ):
+			print( Validation.compare_two_lists( validation_set, train_set, len( validation_set ) ) )
 		else:
-			print( "Dataset and results don't match" )
+			print( "validation_set and train_set don't match" )
 			return
+
+	def compare_two_lists( set_a, set_b, set_len ):
+		matches = 0
+		for i in range( set_len ):
+			if set_a[i][1] == set_b[i][1]:
+				matches = matches + 1
+
+		return matches/float(set_len)
 
 	def load_csv(filename):
 		csv_as_list = list( csv.reader(open( filename, 'rt') ) )
