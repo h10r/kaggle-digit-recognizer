f4e6f6ff5ca71eec699287a9d2d8df7d2b1f3f87
hendrik <hendrikheuer@gmail.com>
2014-02-01 20:58:25 +0100

    added cross validation

--- Classifier/__init__.py
+++ Classifier/__init__.py
@@ -15,7 +15,9 @@ class Classifier():
 		
 		self.load_from_features()
 		
-		self.set_up_classifier()
+		#self.set_up_classifier()
+		
+		self.cross_validation()
 
 	def load_from_features(self):
 		print( "load_from_features" )
@@ -62,13 +64,44 @@ class Classifier():
 		self.predict_testing_set()
 		"""
 
+	def cross_validation(self):
+		print( "cross_validation" )
+
+		cv_X_train, cv_X_test, cv_y_train, cv_y_test = cross_validation.train_test_split( self.X_train,self.y_train, test_size=0.3, random_state=0 )
+
+		print( "SVC: " )
+		self.clf = SVC(gamma=0.001).fit(cv_X_train, cv_y_train)
+		print( clf.score( cv_X_test, cv_y_test ) )
+
+		"""
+		print( "KNeighborsClassifier: " )
+		self.clf = KNeighborsClassifier(3).fit(cv_X_train, cv_y_train)
+		print( clf.score( cv_X_test, cv_y_test ) )
+
+		print( "DecisionTreeClassifier: " )
+		self.clf = DecisionTreeClassifier(max_depth=5).fit(cv_X_train, cv_y_train)
+		print( clf.score( cv_X_test, cv_y_test ) )
+
+		print( "RandomForestClassifier: " )
+		self.clf = RandomForestClassifier(max_depth=5, n_estimators=10, max_features=1).fit(cv_X_train, cv_y_train)
+		print( clf.score( cv_X_test, cv_y_test ) )
+
+		print( "LogisticRegression: " )
+		self.clf = LogisticRegression(C=1e5).fit(cv_X_train, cv_y_train)
+		print( clf.score( cv_X_test, cv_y_test ) )
+
+		print( "GaussianNB: " )
+		self.clf = GaussianNB().fit(cv_X_train, cv_y_train)
+		print( clf.score( cv_X_test, cv_y_test ) )
+
+		print( "LDA: " )
+		self.clf = LDA().fit(cv_X_train, cv_y_train)
+		print( clf.score( cv_X_test, cv_y_test ) )
+		"""
+
 	def predict_testing_set(self):
 		print( "predict_testing_set" )
 
-		for test_sample in self.X_test[3]:
-			print( test_sample )
-
-			clf_predict = self.clf.predict( test_sample )
-
-			print( clf_predict )
+		clf_predict = self.clf.predict( self.X_test )
+		print( clf_predict )
 
