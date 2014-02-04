a58294bc30b3e215863eefd8d5ccd95b9a870274
hendrik <hendrikheuer@gmail.com>
2014-02-04 14:55:04 +0100

    removed validation class

--- main.py
+++ main.py
@@ -2,12 +2,9 @@
 
 from DataSource import *
 from Classifier import *
-from Validation import *
 
 if __name__ == "__main__":
 	d = DataSource()
 	
 	c = Classifier( d )
-
-	#Validation.validate_with_knn( Validation.for_testing() )
 
