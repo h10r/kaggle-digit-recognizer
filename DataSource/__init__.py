f8e0af9ee34734c88def3063665636be8cfc7459
hendrik <hendrikheuer@gmail.com>
2014-02-05 22:54:04 +0100

    turned datasource into singleton

--- DataSource/__init__.py
+++ DataSource/__init__.py
@@ -10,7 +10,7 @@ class DataSource():
 
 	# https://www.kaggle.com/wiki/GettingStartedWithPythonForDataScience/history/969
 
-	def read_data( self, file_name ):
+	def read_data( file_name ):
 		f = open(file_name)
 		
 		#ignore header
@@ -23,7 +23,7 @@ class DataSource():
 			samples.append(sample)
 		return samples
 
-	def write_delimited_file( self, file_path, data, header=None, delimiter="," ):
+	def write_delimited_file( file_path, data, header=None, delimiter="," ):
 		f_out = open(file_path,"w")
 		if header is not None:
 			f_out.write( header + "\n")
@@ -34,13 +34,13 @@ class DataSource():
 				f_out.write( str(line+1) + delimiter + str(value) + "\n")
 		f_out.close()
 
-	def load_train_target_and_test( self ):
-		train = self.read_data( self.PATH_TO_TRAIN )
+	def load_train_target_and_test():
+		train = DataSource.read_data( DataSource.PATH_TO_TRAIN )
 
 		target = [x[0] for x in train]
 		train = [x[1:] for x in train]
 		
-		test = self.read_data( self.PATH_TO_TEST )
+		test = DataSource.read_data( DataSource.PATH_TO_TEST )
 		
 		return train,target,test
 		
